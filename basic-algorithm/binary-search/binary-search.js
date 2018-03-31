10cc9a012813227317d2400016888577033bd165
woai3c <411020382@qq.com>
2018-04-01 00:01:48 +0800

    二分查找算法

---
+++ basic-algorithm/binary-search/binary-search.js
@@ -0,0 +1,16 @@
+// 二分查找算法实现
+function binarySearch(arry, val) {  
+	let start = 0
+	let end = arry.length - 1
+	while (start <= end) {
+		let mid = start + Math.floor((end - start) / 2)
+		if (arry[mid] < val) {
+			start = mid + 1
+		} else if (arry[mid] > val) {
+			end = mid - 1
+		} else {
+			return mid
+		} 
+	} 
+	return -1
+}
