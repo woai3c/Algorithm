ee930d9dd5ff9cc603228c5366bd0e8d63bfedd4
woai3c <411020382@qq.com>
2019-07-18 16:42:51 +0800

    fix bug

--- 02/selection-sort/selection-sort.js
+++ 02/selection-sort/selection-sort.js
@@ -3,7 +3,7 @@
  * @return {void}
  */
 function selectionSort(arry) {
-	const len = arry.length - 1
+	const len = arry.length
 	for (let i = 0, l = len - 1; i < l; i++) {
 		let minIndex = i
 		for (let j = i + 1; j < len; j++) {
