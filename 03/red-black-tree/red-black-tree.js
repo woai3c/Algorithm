0afc5541ea3aad5966f5971b4a1576c136e60df4
woai3c <411020382@qq.com>
2019-08-19 16:08:59 +0800

    完善删除操作 添加测试文件

--- 03/red-black-tree/red-black-tree.js
+++ 03/red-black-tree/red-black-tree.js
@@ -57,9 +57,9 @@ RedBlackTree.prototype.findMax = function(node) {
 function subFindMax(node) {
     if (node.right) {
         return subFindMax(node.right)
-    } else {
-        return node
     }
+
+    return node
 }
 
 // 查找包含最小值的节点
@@ -72,32 +72,69 @@ RedBlackTree.prototype.findMin = function(node) {
 function subFindMin(node) {
     if (node.left) {
         return subFindMin(node.left)
-    } else {
-        return node
-    }
+    } 
+
+    return node
 }
 
 // 删除节点
-RedBlackTree.prototype.delete = function(val, node) {
-    node = node || this.root
-    if (!isRed(node.left) && !isRed(node.right)) root.color = RED
-    node = subDelete(val, node)
-    if (node) node.color = BLACK
+RedBlackTree.prototype.delete = function(val) {
+    if (!this.find(val)) return null
+    const node = this.root
+    if (!isRed(node.left) && !isRed(node.right)) node.color = RED
+    this.root = subDelete(val, node)
+    if (this.root) this.root.color = BLACK
 }
 
 function subDelete(val, node) {
     if (!node) return null
     if (val < node.val) {
+        if (!isRed(node.left) && !isRed(node.left.left)) node = moveRedLeft(node)
         node.left = subDelete(val, node.left)
-    } else if (val > node.val) {
-        node.right = subDelete(val, node.right)
-    } else {
-        if (!node.left) return node.right
-        if (!node.right) return node.left
-        const tempNode = subFindMin(node.right)
-        node.val = tempNode.val
-        node.right = subDelete(tempNode.val, node.right)
+    } else  {
+        if (isRed(node.left)) node = rotateRight(node)
+        if (val == node.val && !node.right) return null 
+        if (!isRed(node.right) && !isRed(node.right.left)) node = moveRedRight(node)
+        if (val == node.val) {
+            const tempNode = subFindMin(node.right)
+            node.val = tempNode.val
+            node.right = subDelete(tempNode.val, node.right)
+        } else {
+            node.right = subDelete(val, node.right)
+        }
     }
+
+    return balance(node)
+}
+
+function moveRedLeft(node) {
+    flipColors(node)
+    // 如果出现两个连续红节点
+    if (isRed(node.right.left)) {
+        node.right = rotateRight(node.right)
+        node = rotateLeft(node)
+        flipColors(node)
+    }
+
+    return node
+}
+
+function moveRedRight(node) {
+    flipColors(node)
+    // 如果出现两个连续红节点
+    if (isRed(node.left.left)) {
+        node = rotateRight(node)
+        flipColors(node)
+    }
+
+    return node
+}
+
+// 删除节点后重新开始平衡树
+function balance(node) {
+    if (isRed(node.right)) node = rotateLeft(node)
+    if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node)
+    if (isRed(node.left) && isRed(node.right)) flipColors(node)
 
     node.size = subGetSize(node.left) + subGetSize(node.right) + 1
     return node
@@ -110,7 +147,6 @@ RedBlackTree.prototype.insert = function(val) {
         return
     }
 
-    if (this.find(val)) return
     this.root = subInsert(this.root, val)
     this.root.color = BLACK
 }
@@ -130,6 +166,7 @@ function subInsert(node, val) {
         }
     }
 
+    // 插入节点后开始平衡树
     if (!isRed(node.left) && isRed(node.right)) node = rotateLeft(node)
     if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node)
     if (isRed(node.left) && isRed(node.right)) flipColors(node)
@@ -186,6 +223,23 @@ function subSort(node, arry) {
     subSort(node.left, arry)
     arry.push(node.val)
     subSort(node.right, arry)
+}
+
+RedBlackTree.prototype.isBalance = function() {
+    let black = 0
+    let node = this.root
+    while (node) {
+        if (!isRed(node)) black++
+        node = node.left
+    }
+
+    return subIsBalance(this.root, black)
+}
+
+function subIsBalance(node, black) {
+    if (!node) return black == 0
+    if (!isRed(node)) black--
+    return subIsBalance(node.left, black) && subIsBalance(node.right, black)
 }
 
 module.exports = RedBlackTree
