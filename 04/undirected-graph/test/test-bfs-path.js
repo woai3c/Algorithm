fac0455ddc6cc21699e87673f1489b46c2fea700
woai3c <411020382@qq.com>
2019-08-28 15:14:46 +0800

    添加广度优先搜索代码

---
+++ 04/undirected-graph/test/test-bfs-path.js
@@ -0,0 +1,24 @@
+const { createGraph } = require('../utils')
+const BreadthFirstPath = require('../breadth-first-path')
+const graph = createGraph('../data/tiny.txt')
+const s = 0
+const search = new BreadthFirstPath(graph, s)
+let str = ''
+for (let v = s, V = graph.getV(); v < V; v++) {
+    str += s + ' to ' + v + ': '
+    if (search.hasPathTo(v)) {
+        const paths = search.pathTo(v)
+        for (let i = paths.length - 1; i >= 0; i--) {
+            const w = paths[i]
+            if (w != s) {
+                str += '-' + w
+            } else {
+                str += w
+            }
+        }
+
+        str += '\n'
+    }
+}
+
+console.log(str)
