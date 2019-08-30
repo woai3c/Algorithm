f45c7a340497ed5b605b74d6ca49fc73fdbfdd3b
woai3c <411020382@qq.com>
2019-08-30 15:50:16 +0800

    添加有向图深度、广度优先路径搜索

---
+++ 04/directed-graph/breadth-first-directed-path.js
@@ -0,0 +1,43 @@
+// 和无向图一样
+function BreadthFirstDirectedPath(graph, s) {
+    this.marked = []
+    this.edgeTo = []
+    this.queue = []
+    this.s = s
+    this.bfs(graph, s)
+}
+
+BreadthFirstDirectedPath.prototype = {
+    bfs(graph, v) {
+        this.queue.push(v)
+        this.marked[v] = true
+        while (this.queue.length) {
+            v = this.queue.shift()
+            graph.getAdj(v).forEach(w => {
+                if (!this.marked[w]) {
+                    this.edgeTo[w] = v
+                    this.marked[w] = true
+                    this.queue.push(w)
+                }
+            })
+        }
+    },
+
+    pathTo(v) {
+        if (!this.hasPathTo(v)) return null
+        const s = this.s
+        const paths = []
+        for (let w = v; w != s; w = this.edgeTo[w]) {
+            paths.push(w)
+        }
+
+        paths.push(s)
+        return paths
+    },
+
+    hasPathTo(v) {
+        return this.marked[v] == true
+    },
+}
+
+module.exports = BreadthFirstDirectedPath
---
+++ 04/directed-graph/test/test-dfs-path.js
@@ -0,0 +1,24 @@
+const { createDigraph } = require('../utils')
+const DepthFirstDirectedPath = require('../depth-first-directed-path')
+const graph = createDigraph('../data/tinyDG.txt')
+const s = 0
+const search = new DepthFirstDirectedPath(graph, s)
+let str = ''
+for (let v = s, V = graph.getV(); v < V; v++) {
+    if (search.hasPathTo(v)) {
+        str += s + ' to ' + v + ': '
+        const paths = search.pathTo(v)
+        for (let i = paths.length - 1; i >= 0; i--) {
+            const w = paths[i]
+            if (w != s) {
+                str += '-' + w
+            } else {
+                str += w
+            }
+        }
+
+        str += '\n'
+    }
+}
+
+console.log(str)
